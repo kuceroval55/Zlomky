d8bfcc4d158811f962df21046d3d496c9a483127
kucerle1 <kucerov33@gmail.com>
2026-02-10 13:03:12 +0100

    Uvodni Commit s hlavni tridou main

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main() {
+        IO.println("============== Projekt Zlomky =============="); // Nove v Java 25 se pise jen IO
+        IO.println("_________");
+    }
+}
