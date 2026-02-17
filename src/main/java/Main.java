74633b2931489db37e33a39c424581c16e9f6981
Leny <kucerov33@gmail.com>
2026-02-17 13:23:52 +0100

    deleno, nasobeni, scitani, odcitani

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,5 +13,32 @@ public class Main {
 
         IO.println(a);
         IO.println(b);
+
+        var soucet = a.plus(b);
+        IO.println("Rozdil je %s".formatted(soucet.zkratit()));
+        IO.println("Rozdil je %s".formatted(a.minus(b).zkratit()));
+        IO.println("Rozdil je %s".formatted(a.krat(b).zkratit()));
+        IO.println("Rozdil je %s".formatted(a.deleno(b).zkratit()));
+
+        Zlomek[] pole =  new Zlomek[]{
+                new Zlomek(1,3),
+                new Zlomek(1,2),
+                new Zlomek(6,5),
+                new Zlomek(4,3),
+                new Zlomek(7,8),
+                new Zlomek(13,10),
+        };
+
+        Zlomek vyslo = prumer(pole);
+        IO.println("Aritmeticky prumer je %s" .formatted(vyslo));
+    }
+
+    private static Zlomek prumer(Zlomek[] pole) {
+        var soucet = new Zlomek(0,1);
+        for  (Zlomek po : pole) {
+            soucet = soucet.plus(po);
+        }
+        //var pocet = new Zlomek(pole.length, 1);
+        return soucet.deleno(pole.length);
     }
 }
--- src/main/java/cz/uhk/zlomky/data/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/data/Zlomek.java
@@ -6,7 +6,10 @@ public class Zlomek {
     private int citatel;
     private int jmenovatel;
 
-    public Zlomek() {} //default konstruktor
+    public Zlomek() {
+        citatel = 0;
+        jmenovatel = 1;
+    } //default konstruktor
 
     public Zlomek(int citatel, int jmenovatel) {
         this.citatel = citatel;
@@ -31,8 +34,58 @@ public class Zlomek {
         this.jmenovatel = jmenovatel;
     }
 
+    public Zlomek plus(Zlomek druhy){
+        int cit = citatel * druhy.jmenovatel + druhy.citatel * jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek minus(Zlomek druhy){
+        int cit = citatel * druhy.jmenovatel - druhy.citatel * jmenovatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek krat(Zlomek druhy){
+        int cit = citatel * druhy.citatel;
+        int jm = jmenovatel * druhy.jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek deleno(Zlomek druhy){
+        int cit = citatel * druhy.jmenovatel;
+        int jm = jmenovatel * druhy.citatel;
+        return new Zlomek(cit, jm);
+    }
+
+    public Zlomek deleno(int num) {
+        return deleno(new Zlomek(num, 1));
+    }
+
+    public Zlomek zkratit (){
+        var delitel = nsd(citatel, jmenovatel);
+        return new Zlomek(citatel / delitel, jmenovatel / delitel);
+    }
+
+    private int nsd(int a,int b){
+        if (a < b) {
+            var prom = a;
+            a = b;
+            b = prom;
+        }
+        // a > b
+        int zb;
+        do{
+            zb = a % b;
+            a = b;
+            b = zb;
+        }while (zb != 0);
+
+        return a;
+    }
+
     @Override
     public String toString() {
-        return String.format("%4d\n________\n%4d",citatel, jmenovatel);
+        return citatel + "/" + jmenovatel; // String.format("%4d\n________\n%4d",citatel, jmenovatel);
     }
 }
